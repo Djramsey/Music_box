915a87ace3bbcdefe3e14b1dd351266f11656692
ScriptEd <scripted@ScriptEd-C02G1066DNKY.local>
2016-08-04 15:43:35 -0400

    initial commit to gh-pages for music_box

---
+++ myscripts.js
@@ -0,0 +1,68 @@
+	var cNote = document.getElementById('cAudio');
+
+	$('#c').mousedown(function(){
+    cNote.play()
+   cNote.currentTime = 0;
+   
+    
+});
+
+
+
+ 
+
+
+
+var dNote = document.getElementById('dAudio');
+
+	$('#d').mousedown(function(){
+    dNote.play()
+   dNote.currentTime = 0;
+   
+});
+
+var eNote = document.getElementById('eAudio');
+
+	$('#e').mousedown(function(){
+    eNote.play()
+   eNote.currentTime = 0;
+    
+});
+
+	var fNote = document.getElementById('fAudio');
+
+	$('#f').mousedown(function(){
+    fNote.play()
+   fNote.currentTime = 0;
+    
+});
+
+	var gNote = document.getElementById('gAudio');
+
+	$('#g').mousedown(function(){
+    gNote.play()
+   gNote.currentTime = 0;
+    
+});
+
+
+var aNote = document.getElementById('aAudio');
+
+	$('#a').mousedown(function(){
+    aNote.play()
+   aNote.currentTime = 0;
+    
+});
+
+var bNote = document.getElementById('bAudio');
+
+	$('#b').mousedown(function(){
+    bNote.play()
+   bNote.currentTime = 0;
+    
+});
+
+
+
+
+
